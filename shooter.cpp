fe52826e17649ace37d3a4279628e7e7fe69d1e1
Jogi91 <jweinbuch@gmx.ch>
2010-12-12 17:16:28 +0100

    Shooter schiesst jetzt

--- shooter.cpp
+++ shooter.cpp
@@ -1,21 +1,30 @@
 #include <iostream>
 #include <string>
+#include <stdlib.h>
 
 using namespace std;
 
 int main(){
 	string input;
 	string command;
-	string duration;
+	string flips;
+	string durationstring;
+	int duration;
 	cout << "Shooter läuft" << endl;
 	while(true){
-		cin >> input
+		cin >> input;
 		if (input == "exit"){
-			cout << "Beende Shooter" << endl
+			cout << "Beende Shooter" << endl;
 		} else {
-			duration = input.substr(1); //Ausser dem ersten Zeichen sind alle Angaben Zeit angaben
+			flips = input[0];
+			command = "echo -n "+flips+" >> /dev/ttyUSB0";
+			system(command.c_str());
+			durationstring = input.substr(1); //Ausser dem ersten Zeichen sind alle Angaben Zeit angaben
+			duration = atoi(durationstring.c_str());
+			usleep(duration*1000);
+			system("echo -n 0 >> /dev/ttyUSB0");
+			
+		}
 	}
-	
-	
 	return 0;
 }
