f4aa6663ddeb858b90527d98434ac0543ad9fe2d
Johannes Weinbuch <jweinbuch@gmx.ch>
2011-01-24 22:47:28 +0100

    Definitive Version

--- main.cpp
+++ main.cpp
@@ -1,6 +1,3 @@
-//Ansatz: Bild und Referenzbild in Schwarzweiss konvertieren, nichtund laufen lassen, runde Form erkennen
-//Das Programm steht bei 0.084s Realzeit, wenn die fenster nicht ge√∂ffnet und die Kreise nicht gezeichnet werden
-
 #include <stdio.h>
 #include "cv.h"
 #include "highgui.h"
@@ -17,18 +14,20 @@ using namespace std;
 #define LINKS  2
 #define RECHTS 1
 #define BEIDE 3
-//Globale Variablen fuer die Referenzbilder
+//Globale Variablen fuer die Referenzbilder, momentan wird nur refFlipsDown verwendet
 IplImage *refFlipsDown = 0;
 IplImage *refRightFlipUp = 0;
 IplImage *refLeftFlipUp = 0;
 IplImage *refBothFlipsUp =0;
 
+//Schiesst, wartet delay millisekunden, und lässt dann wieder los, und das alles mit den gewünschten Flipperfingern
 void shoot(int delay, int flips){
-	cout << flips << delay << endl;
+	cout << flips << delay << endl; //cout wird in den Shooterprozess umgeleitet, siehe unden im Prozesshandling
 }
 
 //nimmt fuer jeden zustand ein Referenzbild auf, und speichert es in den globalen Variablen
-//Return 0 ist Erfolg, alles andere nicht
+//Im Moment wird nur das Referenzbild mit beiden Flippern unten aufgenommen, also nur case 0 erreicht.
+//case 1 ff. sind dazu gedacht, von ausgelösten Flippern Referenzbilder zu schiessen.
 int takeRefShot(IplImage * frame, int& counter){
 	switch (counter) {
 		case 0 :
@@ -49,18 +48,19 @@ int takeRefShot(IplImage * frame, int& counter){
 }
 
 
-//handler f¸r das Beenden durch den Nutzer
+//Signalhandler für das Beenden durch den Nutzer
 void SIGINT_handler (int signum)
 {
 	assert (signum == SIGINT);
 	cerr << "Signal interrupt empfangen" << endl;
-	cout << "exit" << endl;
+	cout << "exit" << endl; //
 	cerr << "vorwait" << endl;
 	wait();
 	cerr << "nachwait" << endl;
 	exit(0);
 }
 
+//Signalhandler für abstürze
 void SIGTERM_handler (int signum) {	
 	assert (signum == SIGTERM);
 	cerr << "SITGTERM empfangen, vermutlich verursacht durch fehlerhaften DV-Stream" << endl;
@@ -69,9 +69,10 @@ void SIGTERM_handler (int signum) {
 	exit(1);
 }
 
+
+// Alte Shoot-Version mit nur 1 Prozess
 //Der Flipper soll delay millisekunden oben bleiben und nur die entsprechenden Flips sollen aktiviert werden.
 // Zuordnoung ist binaer: 01: rchter flipper, 10, linker flipper, 11 beide
-// Version mit nur 1 Prozess
 /*void shoot(int delay, int flips){
 	switch (flips) {
 		case 1:
@@ -131,7 +132,7 @@ int main(){
 		close(commpipe[0]);		/* Close unused side of pipe (in side) */
 		setvbuf(stdout,(char*)NULL,_IONBF,0);	/* Set non-buffered output on stdout */
 		
-
+		//Flipper sagt hallo
 		shoot(100, BEIDE);
 		shoot(100, LINKS);
 		shoot(100, RECHTS);
@@ -152,7 +153,7 @@ int main(){
 		cvNamedWindow( "ball", CV_WINDOW_AUTOSIZE );
 
 		
-		int height, width, step, channels, i, j, k;
+		int height, width, step, channels, i, j;
 		int counter = 0;
 		int mittelxkoordinate;
 		uchar *data;
@@ -206,12 +207,14 @@ int main(){
 			cvWaitKey(250);
 		
 			//Positionsbestimmung des Balls
-			int xkoordinate, ykoordinate, xkoordinatealt, ykoordinatealt;
+			int xkoordinate, ykoordinate, xkoordinatealt, ykoordinatealt, speedx, speedy;
 			ykoordinatealt = 0;
 			xkoordinatealt = 0;
 			xkoordinate = 0;
 			ykoordinate = 0;
 			counter = 0;
+			speedx = 0;
+			speedy = 0;
 			while (1) {
 				//neuer Frame
 				frame = cvQueryFrame( capture );
@@ -224,9 +227,9 @@ int main(){
 				
 
 				
-				cvCvtColor(frame, workingcopy, CV_BGR2GRAY);
-				cvSmooth(workingcopy, workingcopy, CV_GAUSSIAN, 15, 15);
-				//Bild mit nicht XOR verkn√ºpfen
+				cvCvtColor(frame, workingcopy, CV_BGR2GRAY); //Der abgerufene frame wird in die Variable workingcopy kopiert und dabei in GRaustufen konvertiert
+				cvSmooth(workingcopy, workingcopy, CV_GAUSSIAN, 15, 15); //Gausscher weichzeichner
+				//Bild mit nicht XOR verknüpfen
 				for (i = 0; i<height; i++) {
 					for (j = 0; j<width; j++) {
 						if ( j == 0 && i < 10) {
@@ -245,11 +248,11 @@ int main(){
 				
 				//cvShowImage("ball", workingcopy);
 							
-				
+				//Kreise Finden
 				CvSeq* circles =  cvHoughCircles( workingcopy, cstorage, CV_HOUGH_GRADIENT, 1, workingcopy->height/50, 12, 10,4,50 );	
 			
 				//cerr << "Anzahl Gefundener Kreise:" << circles->total << endl;
-				cerr << "Zielkoordinate: " << 3*height/4+50 << endl;
+				//cerr << "Zielkoordinate: " << 375 << endl;
 				
 				xkoordinatealt = xkoordinate;
 				ykoordinatealt = ykoordinate;
@@ -266,10 +269,24 @@ int main(){
 					xkoordinate = xkoordinate/circles->total;
 					ykoordinate	= ykoordinate/circles->total;
 					cerr << "Koordinaten: " << xkoordinate << "/" << ykoordinate << endl;
-					if (ykoordinate == ykoordinatealt) {
+					
+					//Geschwindigkeit berechnen
+					speedx = xkoordinate - xkoordinatealt; //positiv ist bewegung nach rechts
+					speedy = ykoordinate - ykoordinatealt; //positiv ist bewegung nach unten
+					
+					
+					
+					//eingefügt, um zu prüfen, ob das gleiche Bild evtl. mehrmals ausgewertet wird.
+					/*if (ykoordinate == ykoordinatealt) {
 						cerr << "Y-KOORDINATEN GLEICH!!!!" << endl;
 					}
-					if (ykoordinate > 375) {
+					if (xkoordinate == xkoordinatealt) {
+						cerr << "Y-KOORDINATEN GLEICH!!!!" << endl;
+					}*/
+							
+			//Wenn sich die Kugel in der heissen Zone befindet, schiessen, oder wenn sie sich mit der Bewegung in einer heisseren  Zone befinden wird, schiessen
+			//horizontale bewegungen werden noch nicht vorausberechnet
+					if (ykoordinate > 375 or ykoordinate+speedy > 450) {
 						cerr << "ausgelˆst" << endl;
 						counter = 0;
 						if (xkoordinate > width/2 ){
@@ -298,73 +315,3 @@ int main(){
 			
 	return 0;
 }
-
-/*
-//Altes Programm, zum Testen, neues steht oben.
-int main(){
-	int width, height, i, j;
-	IplImage *referenz = 0;
-	IplImage *mitball = 0;
-	
-	int px[0], py[0];
-	CvMemStorage* cstorage = cvCreateMemStorage(0);
-	
-	referenz = cvLoadImage("/Users/johannesweinbuch/Documents/Schule/10:11/matura/ballrecognition/samplepic/sample.jpg",0);
-	mitball = cvLoadImage("/Users/johannesweinbuch/Documents/Schule/10:11/matura/ballrecognition/samplepic/samplewithball.jpg",0);
-	
-	width = referenz -> width;
-	height = referenz -> height;
-	//IplImage* referenzgrau = cvCreateImage(cvSize(width, height), 8, 1);
-	//IplImage* mitballgrau = cvCreateImage(cvSize(width, height), 8, 1);
-
-	//cvCvtColor(referenz, referenzgrau, CV_BGR2GRAY);
-	//cvCvtColor(mitball, mitballgrau, CV_BGR2GRAY);
-	
-	//cvThreshold(referenz, referenz, 25, 255, CV_THRESH_BINARY);
-	//cvThreshold(mitball, mitball, 25, 255, CV_THRESH_BINARY);
-	//cvSmooth(referenz, referenz, CV_GAUSSIAN, 25,25);
-	//cvSmooth(mitball, mitball, CV_GAUSSIAN, 25, 25);
-	
-	cvNamedWindow("referenz", CV_WINDOW_AUTOSIZE);
-	cvNamedWindow("mitball", CV_WINDOW_AUTOSIZE);
-	
-	for (i = 0; i<height; i++) {
-		for (j = 0; j<width; j++) {
-			if ( j == 0 && i < 10) {
-				printf("Pixelwert mitball: %u \n",((uchar *)(mitball->imageData + i*mitball->widthStep))[j]);
-				printf("Pixelwert referenz: %u \n",((uchar *)(referenz->imageData + i*referenz->widthStep))[j]);
-				printf("Pixelwert neu: %u \n\n",~(((uchar *)(mitball->imageData + i*mitball->widthStep))[j] ^ ((uchar *)(referenz->imageData + i*referenz->widthStep))[j]));
-			}
-			((uchar *)(mitball->imageData + i*mitball->widthStep))[j] = ~(((uchar *)(mitball->imageData + i*mitball->widthStep))[j] ^ ((uchar *)(referenz->imageData + i*referenz->widthStep))[j]);
-		}
-	}
-	//cvCanny(mitball, mitball, (float)edge_thresh, (float)edge_thresh*3, 5);
-	//cvSmooth(mitball, mitball, CV_GAUSSIAN, 25,25);
-	cvThreshold(mitball, mitball, 240, 255, CV_THRESH_BINARY);
-    cvSmooth(mitball, mitball, CV_GAUSSIAN, 25, 25);
-
-	CvSeq* circles =  cvHoughCircles( mitball, cstorage, CV_HOUGH_GRADIENT, 2, mitball->height/50, 5, 35,30 );	
-
-	printf("Anzahl Gefundener Kreise: %d",circles->total);
-	
-	for( i = 0; circles->total>=2?i<4:i < circles->total; i++ ){ //just make a filter to limit only <=2 ciecles to draw
-		
-		float* p = (float*)cvGetSeqElem( circles, i );
-		
-		cvCircle( referenz, cvPoint(cvRound(p[0]),cvRound(p[1])), 3, CV_RGB(255,255,255), -1, 8, 0 );
-		
-		cvCircle( referenz, cvPoint(cvRound(p[0]),cvRound(p[1])), cvRound(p[2]), CV_RGB(200,200,200), 1, 8, 0 );
-		
-		px[i]=cvRound(p[0]); py[i]=cvRound(p[1]);
-		
-	}
-	
-	cvShowImage("referenz", referenz);
-	cvShowImage("mitball", mitball);
-	
-	cvWaitKey(0);
-	
-	cvDestroyWindow("referenz");
-	cvDestroyWindow("mitball");
-	return 0;
-}*/
